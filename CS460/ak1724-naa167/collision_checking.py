25575ade1bd58614671507087f3e5e6dffffed0d
AdamKorczak <66699135+adam-korczak@users.noreply.github.com>
2023-10-08 23:14:07 -0400

    Add files via upload

--- CS460/ak1724-naa167/collision_checking.py
+++ CS460/ak1724-naa167/collision_checking.py
@@ -5,16 +5,16 @@ from matplotlib.path import Path
 from create_scene import load_scene, plot_polygons
 
 def check_collision(polygons):
-    collision_flags = [False] * len(polygons)
+    check_collision_path = [False] * len(polygons)
     for i in range(len(polygons)):
         for j in range(len(polygons)):
             if i != j:
                 path = Path(polygons[j])
                 for point in polygons[i]:
                     if path.contains_point(point):
-                        collision_flags[i] = True
-                        collision_flags[j] = True
-    return collision_flags
+                        check_collision_path[i] = True
+                        check_collision_path[j] = True
+    return check_collision_path
 
 def plot_colliding_polygons(polygons):
     fig, ax = plt.subplots(dpi=100)
@@ -25,10 +25,10 @@ def plot_colliding_polygons(polygons):
 
     used_colors = set()
 
-    collision_flags = check_collision(polygons)
+    check_collision_path = check_collision(polygons)
 
     for i, polygon in enumerate(polygons):
-        color = 'red' if collision_flags[i] else 'black'
+        color = 'red' if check_collision_path[i] else 'black'
         polygon_patch = patches.Polygon(polygon, closed=True, fill=True, edgecolor=color, alpha=0.6)
         ax.add_patch(polygon_patch)
 
@@ -36,7 +36,7 @@ def plot_colliding_polygons(polygons):
 
 if __name__ == "__main__":
     filename = input("Enter the filename to load the scene from (default is 'scene.npy'): ") or 'scene.npy'
-    polygons = load_scene(filename, allow_pickle=True)
+    polygons = load_scene(filename)
     #for TAs
     #polygons = np.load(’collision_checking_polygons.npy’, allow_pickle=True)
     plot_colliding_polygons(polygons)
